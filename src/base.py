0574545fbc1a974f655cdf8ade90244bbde097cc
yosemiteyss <kevinhonasdf@gmail.com>
2023-08-25 11:02:10 +0800

    update deployment names

--- src/base.py
+++ src/base.py
@@ -4,10 +4,10 @@ from typing import Optional, List
 
 
 class Model(Enum):
-    GPT35_TURBO = 'gpt-3.5-turbo-0613'
-    GPT35_TURBO_16K = 'gpt-3.5-turbo-16k-0613'
-    GPT4 = 'gpt-4-0613'
-    GPT4_32K = 'gpt-4-32k-0613'
+    GPT35_TURBO = 'gpt-3.5-turbo'
+    GPT35_TURBO_16K = 'gpt-3.5-turbo-16k'
+    GPT4 = 'gpt-4'
+    GPT4_32K = 'gpt-4-32k'
 
 
 class Role(Enum):
