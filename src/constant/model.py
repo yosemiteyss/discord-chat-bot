0fba5839dab6b45de280466d85ef1b137217f478
yosemiteyss <kevinhonasdf@gmail.com>
2023-12-01 16:18:16 +0800

    update model

--- src/constant/model.py
+++ src/constant/model.py
@@ -7,14 +7,14 @@ OPENAI_MODELS: List[Model] = [
     Model(name='gpt-3.5-turbo-16k'),
     Model(name='gpt-4'),
     Model(name='gpt-4-32k'),
+    Model(name='gpt-4-vision-preview', upload_image=True)
 ]
 
 AZURE_MODELS: List[Model] = [
     Model(name='gpt-35-turbo'),
     Model(name='gpt-35-turbo-16k'),
     Model(name='gpt-4'),
-    Model(name='gpt-4-32k'),
-    Model(name='gpt-4-vision-preview', upload_image=True)
+    Model(name='gpt-4-32k')
 ]
 
 PALM_MODELS: List[Model] = [
